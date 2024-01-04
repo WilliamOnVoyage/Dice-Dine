6341fe91000d5a07ec5e73a0fa149ba2ffce08ff
Moliang Zhou <endeavourwilliam.zhou@gmail.com>
2024-01-03 22:51:04 -0800

    [feat]: Add simple authentication method

--- dicedine.py
+++ dicedine.py
@@ -5,6 +5,7 @@ import streamlit as st
 from dicedine.backend.gpt import DiceDineGPT
 from dicedine.backend.map import get_map_df
 from dicedine.utils.logger import MainLogger
+from dicedine.backend.auth import create_authenticator
 
 gpt_client = DiceDineGPT()
 
@@ -25,13 +26,7 @@ def parse_bot_response_address(response_text):
     return addresses
 
 
-# Main App
-def main():
-    st.title("Dice & Dine")
-    # m = folium.Map(location=(37.77, -122.47), tiles="cartodb positron")
-    # # Show maps
-    # st_folium(m, width=725)
-
+def authorized_user_functions():
     # Conversation state
     if 'conversation' not in st.session_state:
         st.session_state.conversation = []
@@ -55,6 +50,21 @@ def main():
     # Display conversation
     for message in st.session_state.conversation:
         st.text(message)
+
+
+# Main App
+def main():
+    st.title("Dice & Dine")
+    authenticator = create_authenticator()
+    name, authentication_status, _ = authenticator.login('Login', 'sidebar')
+    if authentication_status:
+        authenticator.logout('Logout', 'main')
+        st.write(f'Welcome {name}')
+        authorized_user_functions()
+    elif authentication_status is False:
+        st.error('Username/password is incorrect')
+    elif authentication_status is None:
+        st.warning('Please enter your username and password')
 
 
 # Main App
