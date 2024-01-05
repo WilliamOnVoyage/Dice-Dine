1db074feaad9779bd49ff7d56f3fb2677aded8ee
Moliang Zhou <endeavourwilliam.zhou@gmail.com>
2024-01-04 22:42:37 -0800

    [fix]: Install python dependencies after deployment

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -1,6 +1,9 @@
-cd /home/ubuntu/Dice-Dine
+cd /home/ubuntu/Dice-Dine || exit
 
 CONDA_PYTHON="/home/ubuntu/miniconda/bin/python"
+CONDA_PIP="/home/ubuntu/miniconda/bin/pip"
+
+$CONDA_PIP install -r requirements.txt
 $CONDA_PYTHON setup.py build
 $CONDA_PYTHON setup.py install
 
